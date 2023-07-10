01264f3051e5299e5fd5b41da26bd5ae7c0a5e2d
akimabs <kimuwhehe@gmail.com>
2023-07-10 21:25:15 +0700

    improve approval

--- src/api/approval/controllers/approval.ts
+++ src/api/approval/controllers/approval.ts
@@ -3,6 +3,7 @@
  */
 
 import { factories } from "@strapi/strapi";
+import firebaseAdmin from "../../../services/firebase";
 
 export default factories.createCoreController(
   "api::approval.approval",
@@ -42,6 +43,55 @@ export default factories.createCoreController(
     },
 
     async update(ctx) {
+      const id = ctx?.params.id;
+
+      const reimburseUser = await strapi.db
+        .query("api::approval.approval")
+        .findOne({
+          populate: ["reimburses", "*"],
+          where: {
+            id: id,
+          },
+        });
+
+      const idReimburse = reimburseUser.reimburses[0].id;
+
+      const reimburseUserDetail = await strapi.db
+        .query("api::reimburse.reimburse")
+        .findOne({
+          populate: ["user"],
+          where: {
+            id: idReimburse,
+          },
+        });
+
+      const tokenNotif = reimburseUserDetail.user?.tokenNotification;
+      const body = ctx.request.body;
+
+      if (tokenNotif) {
+        firebaseAdmin.messaging().sendToDevice(
+          tokenNotif,
+          {
+            data: {
+              title: `${
+                body?.data?.status === "APPROVE"
+                  ? "✅ Reimbursement disetujui"
+                  : "❌ Reimbursement ditolak"
+              }`,
+              message: `${
+                body?.data?.status === "APPROVE"
+                  ? "Selamat, pengajuan reimburse kamu sudah disetujui"
+                  : "Maaf, pengajuan reimburse kamu ditolak"
+              }`,
+            },
+          },
+          {
+            priority: "high",
+            timeToLive: 60 * 60 * 24,
+          }
+        );
+      }
+
       const response = await super.update(ctx);
       return response;
     },
